dca7b3663ae283e9abfb5c89bb3edfb4735d1f86
Luke Inkster <luke.inkster@xero.com>
2016-08-21 18:09:13 +1200

    Added code to use the preprocessor, lexer and parser

--- Analysis/Program.cs
+++ Analysis/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Antlr4.Runtime;
+using ConsoleApplication1;
 
 namespace CSharpAnalysis
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            using (var fileStream = new StreamReader(@"C:\Dev\CSharpCorpus\AutoMapper\src\AutoMapper\AutoMapperMappingException.cs"))
+            {
+                var inputStream = new AntlrInputStream(fileStream);
+
+                var lexer = new CSharpLexer(inputStream);
+                var commonTokenStream = new CommonTokenStream(lexer);
+                var parser = new CSharpParser(commonTokenStream);
+
+                parser.RemoveErrorListeners();
+                parser.AddErrorListener(new BaseErrorListener());
+
+                var builtParseTree = parser.BuildParseTree;
+                Console.WriteLine(parser.compilation_unit());
+                Console.ReadLine();
+            }
         }
     }
 }

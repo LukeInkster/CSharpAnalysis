84d9680d4d892ad6f8365870e989e07033833760
Luke Inkster <luke.inkster@xero.com>
2016-08-25 12:12:42 +1200

    Improving visitor

--- Analysis/CSharpVisitor.cs
+++ Analysis/CSharpVisitor.cs
@@ -13,24 +13,6 @@ namespace CSharpAnalysis
         public int MethodCount = 0;
         public int VirtualMethodCount = 0;
 
-        public override int VisitMethod_body(CSharpParser.Method_bodyContext context)
-        {
-            Print("hi");
-            return base.VisitMethod_body(context);
-        }
-        public override int VisitMethod_declaration(CSharpParser.Method_declarationContext context)
-        {
-            //Print(context.GetText());
-            return base.VisitMethod_declaration(context);
-        }
-
-        public override int VisitMethod_member_name(CSharpParser.Method_member_nameContext context)
-        {
-            //Print(context.GetText());
-            //Print(context.parent.parent.parent.GetType().ToString());
-            return base.VisitMethod_member_name(context);
-        }
-
         public override int VisitClass_member_declaration(CSharpParser.Class_member_declarationContext context)
         {
             if (DeclarationIsMethod(context))
@@ -41,41 +23,40 @@ namespace CSharpAnalysis
                     VirtualMethodCount++;
                 }
             }
-            //foreach (var parseTree in context.children)
-            //{
-            //    Print("test");
-            //    Print(parseTree.GetType().FullName);
-            //    Print(parseTree.GetText());
-            //}
-            Print(context.GetText());
             return base.VisitClass_member_declaration(context);
         }
 
         private static bool MethodDeclarationIsVirtual(CSharpParser.Class_member_declarationContext context)
         {
-            return ChildrenOf(context.children[0])
+            var result = context
+                .children
+                .OfType<CSharpParser.All_member_modifiersContext>()
+                .SelectMany(ChildrenOf)
                 .Any(IsVirtualModifier);
+            
+            if (result == false && context.GetText().Contains("virtual"))
+            {
+                Print("hi");
+            }
+
+            return result;
         }
 
         private static bool DeclarationIsMethod(CSharpParser.Class_member_declarationContext context)
         {
-            Print(context.GetText());
-
-            var grandChildren = GrandChildrenOf(context);
-
             return GrandChildrenOf(context)
                 .Any(grandChild =>
-                    IsMethodDeclarationNode(grandChild) ||
+                    IsMethodDeclarationTree(grandChild) ||
                         (
                             grandChild is CSharpParser.Typed_member_declarationContext &&
-                            ChildrenOf(grandChild).Any(IsMethodDeclarationNode)
+                            ChildrenOf(grandChild).Any(IsMethodDeclarationTree)
                         )
                     );
         }
 
-        private static bool IsMethodDeclarationNode(IParseTree grandChild)
+        private static bool IsMethodDeclarationTree(IParseTree tree)
         {
-            return grandChild is CSharpParser.Method_declarationContext;
+            return tree is CSharpParser.Method_declarationContext;
         }
 
         private static IEnumerable<IParseTree> ChildrenOf(IParseTree tree)
@@ -91,42 +72,12 @@ namespace CSharpAnalysis
                 .SelectMany(ChildrenOf);
         }
 
-        public override int VisitAll_member_modifier(CSharpParser.All_member_modifierContext context)
-        {
-            //Print(context.GetText());
-            //Print(context.parent.GetType().FullName);
-            //Print(context.GetText());
-            return base.VisitAll_member_modifier(context);
-        }
-
         private static bool IsVirtualModifier(IParseTree child)
         {
             return child
                 .GetText()
                 .ToLower()
                 .Equals("virtual");
-        }
-
-        public override int VisitAll_member_modifiers(CSharpParser.All_member_modifiersContext context)
-        {
-            //Print(context.GetText());
-            //Print("\t" + "\t" + "\t" + "\t" + context.parent.parent.parent.parent.GetType().FullName);
-            //Print("\t" + "\t" + "\t" + context.parent.parent.parent.GetType().FullName);
-            //Print("\t" + "\t" + context.parent.parent.GetType().FullName);
-            //Print("\t" + context.parent.GetType().FullName);
-            //Print(context.GetType().FullName);
-            //Print(context.children.Any(IsVirtualModifier).ToString());
-            return base.VisitAll_member_modifiers(context);
-        }
-
-        public override int VisitClass_member_declarations(CSharpParser.Class_member_declarationsContext context)
-        {
-            //foreach (var decl in context.class_member_declaration())
-            //{
-            //    Print("a " + decl.GetText());
-            //}
-
-            return base.VisitClass_member_declarations(context);
         }
 
         public static void Print(string s)
